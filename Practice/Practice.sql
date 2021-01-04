f81aea53d373062dd76bb3346767ee7da104d15a
xCirno1 <achicir2@gmail.com>
2021-01-04 19:27:56 +0700

    Practice

--- Practice/Practice.sql
+++ Practice/Practice.sql
@@ -1,13 +1,12 @@
 USE master
-
 IF EXISTS (SELECT * FROM sys.databases WHERE Name LIKE 'SellingPoint')
 	DROP DATABASE SellingPoint
 GO
-
 CREATE DATABASE SellingPoint
 GO
-
 USE SellingPoint
+
+-------------------------------------------
 
 CREATE TABLE Categories (
 	CateID char(6) CONSTRAINT PK_cateid PRIMARY KEY (CateID),
@@ -25,23 +24,38 @@ CREATE TABLE Parts (
 	Warranty int DEFAULT (1),
 	Photo nvarchar(200) DEFAULT ('photo/nophoto.png')
 )
+GO
+
+-------------------------------------------
 
 INSERT INTO Categories VALUES
--- Thêm vào đây
+	('CPU','CPU','Computer`s CPU'),
+	('RAM','RAM','Computer`s RAM'),
+	('HDD','HDD','Computer`s HDD')
 
 INSERT INTO Parts VALUES
--- Thêm vào đây
+	('HDD 100GB','HDD',N'HDD có 100GB',500,50,1,default),
+	('RAM 2GB','RAM',N'RAM có 2GB',900,50,1,default),
+	('God CPU','CPU',N'God`s CPU',9999999,1,9999,default)
+GO
 
+-------------------------------------------
 
 SELECT * FROM Parts WHERE Price > 100
 
-SELECT * FROM Parts WHERE CateID LIKE 'CPU'
+SELECT * FROM Parts JOIN Categories on Categories.CateID = Parts.CateID WHERE CateName LIKE 'CPU'
 GO
+
+-------------------------------------------
 
 CREATE VIEW V_Parts AS
 SELECT PartID, PartName, Price, Quantity FROM Categories JOIN Parts on Categories.CateID = Parts.CateID
 GO
 
+-------------------------------------------
+
 CREATE VIEW V_TopParts AS
 SELECT TOP 5 * FROM Parts ORDER BY Price DESC
 GO
+
+-------------------------------------------
